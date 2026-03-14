601a6342797276d5129f89567adef378e3df0b4d
Gibson Agbor <mcerimainterltd@gmail.com>
2026-03-14 04:38:45 +0100

    Remove androidScheme, add WebView data directory suffix

--- android/app/src/main/java/com/xamepage/app/MainActivity.java
+++ android/app/src/main/java/com/xamepage/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
