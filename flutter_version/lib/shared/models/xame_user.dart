95403f1b891e687f2ced7e16bd93e1b495a8ee59
Gibson Agbor <mcerimainterltd@gmail.com>
2026-04-02 23:36:58 +0100

    FIX: Build 28 - Added Dio/Riverpod and synced XameUser model

--- flutter_version/lib/shared/models/xame_user.dart
+++ flutter_version/lib/shared/models/xame_user.dart
@@ -3,15 +3,27 @@ class XameUser {
   final String firstName;
   final String lastName;
   final String? email;
+  final String? phone;
+  final String? profilePic;
+  final String? sessionToken;
 
-  XameUser({required this.xameId, required this.firstName, required this.lastName, this.email});
+  const XameUser({
+    required this.xameId,
+    required this.firstName,
+    required this.lastName,
+    this.email,
+    this.phone,
+    this.profilePic,
+    this.sessionToken,
+  });
 
-  factory XameUser.fromMap(Map<String, dynamic> map) {
-    return XameUser(
-      xameId: map['xameId'] ?? '',
-      firstName: map['firstName'] ?? '',
-      lastName: map['lastName'] ?? '',
-      email: map['email'],
-    );
-  }
+  factory XameUser.fromMap(Map<String, dynamic> m) => XameUser(
+    xameId: m['xameId'] as String,
+    firstName: m['firstName'] as String? ?? '',
+    lastName: m['lastName'] as String? ?? '',
+    email: m['email'] as String?,
+    phone: m['phone'] as String?,
+    profilePic: m['profilePic'] as String?,
+    sessionToken: m['sessionToken'] as String?,
+  );
 }
