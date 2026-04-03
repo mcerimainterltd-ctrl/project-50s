54b7236562ea0a9054bdd2bd9b43320e944a3ba3
Gibson Agbor <mcerimainterltd@gmail.com>
2026-04-03 13:30:36 +0100

    FIX: Build 33 - Corrected nested paths, removed duplicate services, and updated dependencies

--- flutter_version/lib/screens/login_screen.dart
+++ flutter_version/lib/screens/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../services/auth_service.dart';
+import '../core/services/auth_service.dart';
 import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
